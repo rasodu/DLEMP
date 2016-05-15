342631a57ab701375b6c1a8aa6384b7dd3c19019
Abhijit Amin <abhijitramin@gmail.com>
2016-05-15 12:21:05 -0400

    Unittests are added.

--- tests/A00PHPDlempTest.php
+++ tests/A00PHPDlempTest.php
@@ -2,6 +2,16 @@
 
 class A00PHPDlempTest extends TestCase
 {
+    public function testCustomProductionSettings(){
+        $this->assertSame('GPCS', ini_get('variables_order'));
+        $this->assertSame('GP', ini_get('request_order'));
+    }
+
+    public function testCustomDevelopmentSettings(){
+        $this->assertSame(strval(E_ALL), ini_get('error_reporting'));
+        $this->assertSame('1', ini_get('display_errors'));
+        $this->assertSame('1', ini_get('display_startup_errors'));
+    }
 
     public function testExtensionCurl()
     {
@@ -19,5 +29,13 @@ class A00PHPDlempTest extends TestCase
             $this->isExtensionLoaded('xdebug'),
             "Xdebug is not installed. You will not be able to generate PHPUnit code coverage report."
         );
+
+        $this->assertSame('1', ini_get('xdebug.remote_enable'));
+        $this->assertSame('1', ini_get('xdebug.remote_connect_back'));
+        $this->assertSame('9000', ini_get('xdebug.remote_port'));
+        $this->assertSame('dbgp', ini_get('xdebug.remote_handler'));
+        $this->assertSame('req', ini_get('xdebug.remote_mode'));
+        $this->assertSame('1', ini_get('xdebug.cli_color'));
+        $this->assertSame('2', ini_get('xdebug.overload_var_dump'));
     }
 }
