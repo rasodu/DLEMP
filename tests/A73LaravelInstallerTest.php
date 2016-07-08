ca99f7cde2059833c03034d7a0b4fedfbd3cf6e6
Abhijit Amin <abhijitramin@gmail.com>
2016-07-08 06:26:18 -0400

    Unittest checking installation of Laravel Installer is added.

---
+++ tests/A73LaravelInstallerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use Symfony\Component\Process\Process;
+
+class A73LaravelInstallerTest extends TestCase
+{
+
+    public function testLaravelCommandworks()
+    {
+        $process = new Process('~/.composer/vendor/bin/laravel --version');
+        $process->run();
+        $output= $process->getOutput();
+
+        $this->assertContains("Laravel Installer version", $output);
+    }
+}
