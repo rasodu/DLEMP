91a975a57d4acd4f8df724007b5fe064007b450c
Abhijit Amin <abhijitramin@gmail.com>
2017-03-12 01:20:28 -0500

    Make a small adjustment to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,24 @@
 var gulp = require('gulp'),
     watch = require('gulp-watch'),
     batch = require('gulp-batch'),
+    runSequence = require('run-sequence'),
     exec = require('child_process').exec;
 
+gulp.task('test', function(done) {
+    console.log('Statring command: \`make test\`');
+
+    var makeProcess = exec('make test');
+    makeProcess.stdout.pipe(process.stdout);
+    makeProcess.stderr.pipe(process.stderr);
+    makeProcess.on('exit', function(code) {
+        console.log('Ending command: \`make test\`');
+        done(code);
+    });
+});
+
 gulp.task('test-watch', function() {
+    runSequence('test');
     return watch(['docker/**', 'nodejs/**', 'public/**', 'resources/**', 'tests/**', '.dockerignore', '.env', 'artisan', 'compose.*', 'composer.json', 'phpunit.xml'], batch(function(events, done) {
-        console.log('Statring command: \`make test\`');
-
-        var makeProcess = exec('make test');
-        makeProcess.stdout.pipe(process.stdout);
-        makeProcess.stderr.pipe(process.stderr);
-        makeProcess.on('exit', function(code) {
-            console.log('Ending command: \`make test\`');
-            done(code);
-        });
-
+        runSequence('test', done);
     }));
 });
